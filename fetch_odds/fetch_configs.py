5ba7014e303042cc88aa8dc94f55537ec94bc7f1
Andrew Smith <asmith42@unca.edu>
2025-09-23 01:21:27 -0400

    remove redundant api key variable

--- fetch_odds/fetch_configs.py
+++ fetch_odds/fetch_configs.py
@@ -5,9 +5,6 @@ Centralized variables for fetch_odds folder.
 
 Author: Andrew Smith
 """
-# API key
-THEODDS_API_KEY = "9e971bc2de30ec1e46baa52d8cf4dc92"
-
 # Available Sports Keys
 THEODDS_SPORTS_DICT = {
     "upcoming": "upcoming",
