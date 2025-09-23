70f29ef0fcb7b2195bc4d2149b44220b8e8e5dd4
Andrew Smith <asmith42@unca.edu>
2025-09-23 01:20:13 -0400

    detailed variable comments

--- results/results_configs.py
+++ results/results_configs.py
@@ -7,12 +7,16 @@ Author: Andrew Smith
 """
 from zoneinfo import ZoneInfo
 
-# Configuration
+# Local timezone for measuring how long it has been since each bet
 TIMEZONE = ZoneInfo("America/New_York")
+
+# How many days back in The-Odds_API to look back for results (max of 3 days)
 DAYS_CUTOFF = 3
+
+# How long to sleep after TheSportsDB request limit (30 requests/min)
 SLEEP_DURATION = 60
 
-# File configurations
+# File names
 FILE_CONFIGS = [
     ("master_avg_bets.csv", "master_avg_full.csv"),
     ("master_mod_zscore_bets.csv", "master_mod_zscore_full.csv"),
@@ -21,4 +25,5 @@ FILE_CONFIGS = [
     ("master_random_bets.csv", "master_random_full.csv"),
 ]
 
+# Unfinished results
 PENDING_RESULTS = ["Not Found", "Pending", "API Error"]
